66e5744685f1b1d880a9457ceb31980357eda9e8
kalyani-pujari <kalyanipujari118@gmail.com>
2024-05-11 13:03:06 +0530

    added factorial.java

---
+++ factorial.java
@@ -0,0 +1,17 @@
+
+public class factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 5;
+		long factorial = 1;
+		
+		for( int i=1; i<=num; i++) {
+		factorial = factorial*i;
+		}
+		System.out.println("factorial of number is" +factorial);
+		
+	}
+
+}
